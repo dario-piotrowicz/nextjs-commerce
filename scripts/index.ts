c58991c55c7487fafb1d650c9c4111ad7d83fcae
Michael Hart <mhart@cloudflare.com>
2024-08-16 23:17:37 +1000

    Explicitly cache images just in case

--- scripts/index.ts
+++ scripts/index.ts
@@ -25,7 +25,10 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname === '/_next/image') {
-      return fetch(url.searchParams.get('url') ?? 'http://www.apple.com');
+      return fetch(
+        url.searchParams.get('url') ?? 'https://developers.cloudflare.com/_astro/logo.BU9hiExz.svg',
+        { cf: { cacheEverything: true } } as any
+      );
     }
 
     const resBody = new TransformStream();
